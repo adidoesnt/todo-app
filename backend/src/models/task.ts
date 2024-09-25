063c66d87012e7041f450927d15f32bcb51c04fc
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-09-25 14:59:45 +0800

    [BE] add task model

---
+++ backend/src/models/task.ts
@@ -0,0 +1,24 @@
+import { getModelForClass, prop } from "@typegoose/typegoose";
+
+export enum TaskStatus {
+    TODO = 'to do',
+    IN_PROGRESS = 'in progress',
+    DONE = 'done',
+    MISSED = 'missed',
+}
+
+export class TaskSchema {
+    @prop({ required: true, type: String })
+    declare name: string;
+
+    @prop({ required: true, type: String })
+    declare description: string;
+
+    @prop({ required: true, type: Date })
+    declare deadline: Date;
+
+    @prop({ required: true, type: String })
+    declare status: TaskStatus;
+}
+
+export const Task = getModelForClass(TaskSchema);
